88790be686adb894f84290abe2476c90aa3f2536
Rafael Papa <rafaelpapa81@gmail.com>
2025-01-05 23:22:32 -0500

    gp-killer move app to src folder

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: [
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  // ... rest of your config
+} 
